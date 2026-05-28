bd8a24028dd3552dce92d1cf2bd9ed6467f44778
Boban Milošević <milosevic.boban@gmail.com>
2026-05-29 04:57:57 +0700

    🗑️ Removed CORS from API

--- apps/api/src/create-app.ts
+++ apps/api/src/create-app.ts
@@ -1,12 +1,9 @@
 import { Hono } from 'hono'
-import { cors } from 'hono/cors'
 import { HTTPException } from 'hono/http-exception'
 
 export function createApp() {
 
     const app = new Hono()
-
-    app.use(cors())
 
     app.onError((err, ctx) => {
         console.error(err)
